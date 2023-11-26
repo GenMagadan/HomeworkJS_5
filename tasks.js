7f59525b0ec9f9b7df9175ae5b566483593e15f1
GenMagadan <genchik111@yandex.ru>
2023-11-26 19:26:59 +0300

    Added task_2

--- tasks.js
+++ tasks.js
@@ -1,18 +1,52 @@
 // 1. Дан объект numbers. Необходимо в консоль вывести все
 // значения больше или равные 3.
 
-const numbers = {
-   keyin1: 1,
-   keyin2: 2,
-   keyin3: 3,
-   keyin4: 4,
-   keyin5: 5,
-   keyin6: 6,
-   keyin7: 7,
-}
+// const numbers = {
+//    keyin1: 1,
+//    keyin2: 2,
+//    keyin3: 3,
+//    keyin4: 4,
+//    keyin5: 5,
+//    keyin6: 6,
+//    keyin7: 7,
+// }
 
-for (const key in numbers) {
-   if (numbers[key] >= 3) {
-      console.log(`${[key]} - ${numbers[key]}`);
-   }
-}
+// for (const key in numbers) {
+//    if (numbers[key] >= 3) {
+//       console.log(`${[key]} - ${numbers[key]}`);
+//    }
+// }
+
+// 2. Необходимо из объекта, который лежит в константе post
+// вывести значения, к которым приписан комментарий, в консоль.
+
+const post = {
+   author: "John", // вывести этот текст
+   postId: 23,
+   comments: [
+      {
+         userId: 10,
+         userName: "Alex",
+         text: "lorem ipsum",
+         rating: {
+            likes: 10,
+            dislikes: 2, // вывести это число
+         },
+      },
+      {
+         userId: 5, // вывести это число
+         userName: "Jane",
+         text: "lorem ipsum 2", // вывести этот текст
+         rating: {
+            likes: 3,
+            dislikes: 1,
+         },
+      },
+   ],
+};
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
+
