25f164921aafa95340df2a7d89c322514494456e
GenMagadan <genchik111@yandex.ru>
2023-11-26 19:54:46 +0300

    Added task_3

--- tasks.js
+++ tasks.js
@@ -20,33 +20,55 @@
 // 2. Необходимо из объекта, который лежит в константе post
 // вывести значения, к которым приписан комментарий, в консоль.
 
-const post = {
-   author: "John", // вывести этот текст
-   postId: 23,
-   comments: [
-      {
-         userId: 10,
-         userName: "Alex",
-         text: "lorem ipsum",
-         rating: {
-            likes: 10,
-            dislikes: 2, // вывести это число
-         },
-      },
-      {
-         userId: 5, // вывести это число
-         userName: "Jane",
-         text: "lorem ipsum 2", // вывести этот текст
-         rating: {
-            likes: 3,
-            dislikes: 1,
-         },
-      },
-   ],
-};
+// const post = {
+//    author: "John", // вывести этот текст
+//    postId: 23,
+//    comments: [
+//       {
+//          userId: 10,
+//          userName: "Alex",
+//          text: "lorem ipsum",
+//          rating: {
+//             likes: 10,
+//             dislikes: 2, // вывести это число
+//          },
+//       },
+//       {
+//          userId: 5, // вывести это число
+//          userName: "Jane",
+//          text: "lorem ipsum 2", // вывести этот текст
+//          rating: {
+//             likes: 3,
+//             dislikes: 1,
+//          },
+//       },
+//    ],
+// };
 
-console.log(post.author);
-console.log(post.comments[0].rating.dislikes);
-console.log(post.comments[1].userId);
-console.log(post.comments[1].text);
+// console.log(post.author);
+// console.log(post.comments[0].rating.dislikes);
+// console.log(post.comments[1].userId);
+// console.log(post.comments[1].text);
 
+// 3. Дан массив products, необходимо цену каждого продукта
+// уменьшить на 15% используя метод forEach.
+
+const products = [
+   {
+      id: 3,
+      price: 200,
+   },
+   {
+      id: 4,
+      price: 900,
+   },
+   {
+      id: 1,
+      price: 1000,
+   },
+];
+
+products.forEach(element => {
+   element.price -= element.price * (15 / 100);
+});
+console.log(products);
